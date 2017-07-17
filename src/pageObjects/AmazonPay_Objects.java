6b4b398f162ccb387b7ef6f1a551aba1eb887c64
Suhail Dandu <ruchi.gulati@vinsolutions.com>
2017-07-17 00:45:42 -0700

    Just to check Git Push

--- src/pageObjects/AmazonPay_Objects.java
+++ src/pageObjects/AmazonPay_Objects.java
@@ -2,6 +2,8 @@ package pageObjects;
 
 import org.openqa.selenium.By;
 
+//Comment to check git Push
+
 public class AmazonPay_Objects {
 
 	public static By img_ViewStmt = By.xpath("//img[@alt='View Statement']");
